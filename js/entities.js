b3f2e1878ac9e47ff2da6679d6edee154c35f18d
Giles Lavelle <giles.lavelle@gmail.com>
2012-02-23 19:02:08 +0000

    Improved keyboard controls

--- js/entities.js
+++ js/entities.js
@@ -31,28 +31,28 @@ randColour = function(){
     return Math.floor(Math.random() * 16777215);
 };
 
-var Player = function(p){
-    this.arrayPosition = new THREE.Vector3(p.x, p.y, p.z);
+var Entity = function(){
     this.position = new THREE.Vector3();
-    this.position.addSelf(this.arrayPosition);
-    this.position.multiplyScalar(cubeSize);
-    // this.speed = new THREE.Vector3();
-    // this.rotation = {
-    //     speed : new THREE.Vector3(),
-    //     angle : new THREE.Vector3()
-    // };
-    //this.accel = new THREE.Vector3(0, -9.8, 0);
-    //this.friction = new THREE.Vector3();
-    //this.step = 0.1;
-    //this.maxSpeed = 0.2;
     this.animationIncrement = cubeSize / 10;
     this.isAnimating = false;
     this.distanceToMove = 0;
 
-    this.colour = 0x0000ff;
 };
 
-var Animal = function(d){
+Player.prototype = new Entity();
+Player.prototype.constructor = Player;
+
+Animal.prototype = new Entity();
+Animal.prototype.constructor = Animal;
+
+function Player(p){
+    this.arrayPosition = new THREE.Vector3(p.x, p.y, p.z);
+    this.position.addSelf(this.arrayPosition);
+    this.position.multiplyScalar(cubeSize);
+    this.colour = 0xff0000;
+}
+
+function Animal(d){
     this.arrayPosition = new THREE.Vector3(
         THREE.Math.randInt(2, d.x - 3),
         1,
@@ -66,19 +66,7 @@ var Animal = function(d){
     this.colour = randColour();
     this.height = cubeSize;
     this.width = cubeSize;
-
-    this.animationIncrement = cubeSize / 10;
-    this.isAnimating = false;
-    this.distanceToMove = 0;
-    // this.angle = Math.random() * tau;
-    // console.log(this.angle);
-    // this.speed = new THREE.Vector3(
-    //     (Math.random() - 0.5) * 0.01,
-    //     0,
-    //     (Math.random() - 0.5) * 0.01
-    // );
-    // this.accel = -1;
-};
+}
 
 var Goal = function(p){
     this.arrayPosition = new THREE.Vector3(p.x, p.y, p.z);
--- js/main.js
+++ js/main.js
@@ -9,6 +9,9 @@ $(document).ready(function(){
         cubes = [],
         cameraOffset = new THREE.Vector3(4, 6, 7).multiplyScalar(cubeSize);
 
+    var keyboard = new THREEx.KeyboardState(),
+        settings;
+
     //var mouse, projector, ray, floor;
 
     var fps,
@@ -136,6 +139,8 @@ $(document).ready(function(){
 
         var i;
 
+        checkKeyboard();
+
         tick = tick > 100 ? 1 : tick + 1;
         fps = Math.round(1000 / (time - oldTime));
         oldTime = time;
@@ -255,43 +260,51 @@ $(document).ready(function(){
         camera.lookAt(playerdata.position);
     };
 
-    var bindInputs = function(){
+    var checkKeyboard = function(){
         //Try and bind these keys in a loop, lots of duplication here
         //Couldn't get it to work previously.
 
-        $.getJSON('settings.json', function(data){
-            var k = data.keyboard.movement;
-            key(k.forwards, function(){
+        var k = settings.keyboard.movement;
+
+        if (keyboard.pressed(k.forwards)){
                 updatePos(playerdata, 'z', -1);
-            });
-
-            key(k.left, function(){
+        }
+        if (keyboard.pressed(k.left)){
                 updatePos(playerdata, 'x', -1);
-            });
-
-            key(k.back, function(){
+        }
+        if (keyboard.pressed(k.back)){
                 updatePos(playerdata, 'z', 1);
-            });
-
-            key(k.right, function(){
+        }
+        if (keyboard.pressed(k.right)){
                 updatePos(playerdata, 'x', 1);
-            });
+        }
+        if (keyboard.pressed(settings.keyboard.jump.up)){
+            var p = playerdata.arrayPosition;
+            //Check if there's a block underneath before letting the player jump
+            if(flying || (p.y === 0 || world[p.y - 1][p.x].charAt(p.z) !== '0')){
+                updatePos(playerdata, 'y', 1);
+            }
+        }
+        if (keyboard.pressed(settings.keyboard.jump.down)){
+            updatePos(playerdata, 'y', -1);
+        }
+        if (keyboard.pressed(settings.keyboard.misc.pause)){
+            updatePos(playerdata, 'y', -1);
+        }
+        if (keyboard.pressed(settings.keyboard.fly)){
+            updatePos(playerdata, 'y', -1);
+        }
+    };
 
-            key(data.keyboard.jump.up, function(){
-                var p = playerdata.arrayPosition;
-                //Check if there's a block underneath before letting the player jump
-                if(flying || (p.y === 0 || world[p.y - 1][p.x].charAt(p.z) !== '0')){
-                    updatePos(playerdata, 'y', 1);
-                }
-            });
+    var bindInputs = function(){
+        //Bind keys that perform a distinct action.
+        //Control keys for player movement need more precise control
 
-            key(data.keyboard.jump.down, function(){
-                updatePos(playerdata, 'y', -1);
-            });
+        $.getJSON('settings.json', function(data){
+            settings = data;
 
             key(data.keyboard.misc.pause, function(){ paused = !paused; });
             key(data.keyboard.fly, function(){ flying = !flying; });
-
         });
     };
 
